9a3c65e4532686fe7e68f0a126104416491a4c2b
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2022-08-05 13:01:59 -0400

    fetch image results from Pixabay API and save results with useState and set default maxResults to 12

--- src/components/ImagesContainer.js
+++ src/components/ImagesContainer.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageSearch from '../components/ImageSearch';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-const IMAGE_API = "https://pixabay.com/api/" + API_KEY;
-
-const fetchImages = (input) => {
-  let query = input.split(' ').join('+');
-  let apiUrl = IMAGE_API.concat(`&q=${query}`);
-  console.log(apiUrl);
-  // fetch(apiUrl)
-  //   .then(resp => resp.json())
-  //   .then(imagesData => {
-
-  //   })
-};
+const IMAGE_API = "https://pixabay.com/api/" + `?key=${API_KEY}`;
 
 export default function ImageContainer() {
+  const [images, setImages] = useState([])
+  const [maxResults, setMaxResults] = useState('12');
+
+  const fetchImages = (input) => {
+    let query = input.split(' ').join('+');
+    let imagesOnly = '&image_type=photo';
+    let resultsPerPage = `&per_page=${maxResults}`
+    let apiUrl = IMAGE_API.concat(`&q=${query}`, imagesOnly, resultsPerPage);
+
+    fetch(apiUrl)
+      .then(resp => resp.json())
+      .then(imagesData => {
+        if (imagesData.totalHits > 0) {
+          setImages(imagesData.hits);
+          console.log(imagesData.hits);
+        } else {
+          setImages([]);
+        }
+
+      })
+  };
+
   return (
     <ImageSearch onSubmit={fetchImages} />
   );
